771a35e1ed9144ccc3700f96c789b897c211fc91
Matthew Hately <blake@matthews-mbp.attlocal.net>
2018-06-24 03:30:28 -0400

    Initial Commit

---
+++ gitExampleXC9/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gitExampleXC9
+//
+//  Created by Matthew Hately on 6/24/18.
+//  Copyright © 2018 matthewhately. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gitExampleXC9/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gitExampleXC9
+//
+//  Created by Matthew Hately on 6/24/18.
+//  Copyright © 2018 matthewhately. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
